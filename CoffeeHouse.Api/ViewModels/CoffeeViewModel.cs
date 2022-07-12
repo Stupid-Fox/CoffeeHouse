ce1c155c71f1f1c1268e099d7bb178f928412d30
Владислав Чеховский <chehowskij.vladim@yandex.ru>
2022-07-12 15:16:16 +0300

    Add

---
+++ CoffeeHouse.Api/ViewModels/CoffeeViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CoffeeHouse.Api.ViewModels
+{
+    public class CoffeeViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "Coffee";
+        public int Cost { get; set; }
+        public int Temperature { get; set; }
+        public int MakeTime { get; set; }
+    }
+}
